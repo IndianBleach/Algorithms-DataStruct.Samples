96b1cd2c43a72cf2032a380829e9c984fc00638d
indianbleach <84047808+IndianBleach@users.noreply.github.com>
2022-08-01 22:18:37 +0300

    add LinearSearch and KmpStringPatternsSearch

--- src/Output/Program.cs
+++ src/Output/Program.cs
@@ -32,12 +32,88 @@ bool IsPriveNumber(int n)
     return true;
 }
 
+int LinearSearch(int key, int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == i)
+            return i;
+    }
+    return -1;
+}
+
+
+//https://proglib.io/p/6-search-algorithms-java
+int[] GetPatternStringArray(string pattern)
+{   
+    int len = pattern.Length;
+    int[] res = new int[len];
+    int i = 1;
+    int cur = 0;
+
+    res[0] = 0;
+
+    while (cur < len)
+    {
+        if (pattern[i] == pattern[cur])
+        {
+            cur++;
+            i++;
+            res[i] = cur;
+        }
+        else
+        {
+            if (cur != 0)
+                cur = res[cur - 1];
+            else
+            {
+                i++;
+                res[i] = cur;
+            }
+        }
+    }
+    return res;
+}
+
+List<int> PerformKMPStringPatternSearch(string searchText, string pattern)
+{
+    int[] patternArray = GetPatternStringArray(pattern);
+
+    int textIndex = 0;
+    int patternIndex = 0;
+
+    List<int> founds = new();
+
+    while (textIndex < searchText.Length)
+    {
+        if (pattern[patternIndex] == searchText[textIndex])
+        {
+            patternIndex++;
+            textIndex++;
+        }
+
+        if (patternIndex == pattern.Length)
+        {
+            founds.Add(textIndex - patternIndex);
+            patternIndex = patternArray[patternIndex - 1];
+        }
+        else if (textIndex < searchText.Length && pattern[patternIndex] != searchText[textIndex])
+        {
+            if (patternIndex != 0)
+            {
+                patternIndex = patternArray[patternIndex - 1];
+            }
+            else
+            {
+                textIndex++;
+            }
+        }
+    }
+
+    return founds;
+}
 
 for (int i = 0; i < 300; i++)
 {
     Console.WriteLine(String.Format("{0}, {1}", i, IsPriveNumber(i)));
 }
-
-
-
-
